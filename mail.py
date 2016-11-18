f3d73880b1bf54eb2d619c5e742416f0e9b4d049
phil3k <philip.limbeck@gmail.com>
2016-11-18 17:31:02 +0100

    removed argparse

--- mail.py
+++ mail.py
@@ -12,12 +12,6 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-try:
-    import argparse
-
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
